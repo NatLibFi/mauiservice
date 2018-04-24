3d5fed59a5526ddf7120d8607af734fdd37ae03b
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-04-24 11:43:52 +0300

    Ensure mauiservice wars are actually deployable

--- src/main/java/fi/nationallibrary/mauiservice/Application.java
+++ src/main/java/fi/nationallibrary/mauiservice/Application.java
@@ -26,9 +26,10 @@ package fi.nationallibrary.mauiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	
 	public static void main(String...args) {
 		SpringApplication.run(Application.class, args);
